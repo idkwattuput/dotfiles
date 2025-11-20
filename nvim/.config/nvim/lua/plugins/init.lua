4da69b7e3bf3d7027544f90b8596e48519a4b03d
idkwattuput <ikhwank322@gmail.com>
2025-11-20 12:55:18 +0800

    add wakatime

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -47,6 +47,8 @@ return {
         "cpp",
         "make",
       },
+      highlight = { enable = true },
+      indent = { enable = true },
     },
   },
   {
@@ -97,13 +99,26 @@ return {
     event = "VeryLazy",
     ---@type Flash.Config
     opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
-  },
+  { "wakatime/vim-wakatime", lazy = false },
+  -- {
+  --   "JoosepAlviste/nvim-ts-context-commentstring",
+  --   enable_autocmd = false,
+  --   languages = {
+  --     typescript = "// %s",
+  --   },
+  -- },
+  -- {
+  --   "numToStr/Comment.nvim",
+  --   opts = {},
+  --   pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+  -- },
 }
