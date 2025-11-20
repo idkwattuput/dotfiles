1a3896c0c30c478980cc7d75d7f6774aceae2ada
idkwattuput <ikhwank322@gmail.com>
2025-11-20 12:56:01 +0800

    add dotfile and exercism

--- start-tmux/.local/bin/start-tmux.sh
+++ start-tmux/.local/bin/start-tmux.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/ ~/Projects ~/Playground ~/.config/ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/ ~/dotfiles/ ~/.config ~/exercism/ ~/Projects ~/Playground ~/.config/ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
